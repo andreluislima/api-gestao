7820647c0420532d77f2d4f43829302761aaf77a
andreluislima <andrelimag89@gmail.com>
2025-11-10 12:13:16 -0300

    feat: cria componente brand e login

--- front-end/app-gestao/src/app/components/login/login.ts
+++ front-end/app-gestao/src/app/components/login/login.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormLogin } from "./form-login/form-login";
+import { Brand } from "../brand/brand";
 
 @Component({
   selector: 'app-login',
-  imports: [FormLogin],
+  imports: [FormLogin, Brand],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
