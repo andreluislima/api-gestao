a52ed3f8829aced7abe51e7f6ebee6a01eddb1b5
andreluislima <andrelimag89@gmail.com>
2025-12-01 14:41:54 -0300

    feat: cria comportamento 'sanduiche'.

--- front-end/app-gestao/src/app/layout/default-layout/default-layout.ts
+++ front-end/app-gestao/src/app/layout/default-layout/default-layout.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-default-layout',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './default-layout.html',
   styleUrl: './default-layout.scss',
   encapsulation: ViewEncapsulation.None
 })
 export class DefaultLayout {
 
+  sidebarToggled = false;
+
+  toggleSidebar():void{
+    this.sidebarToggled = !this.sidebarToggled;
+  }
+   
 }
