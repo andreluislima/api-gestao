d2a34ce48af8e75bae84b9ae4c04d3d357d38180
andreluislima <andrelimag89@gmail.com>
2025-12-08 20:19:34 -0300

    feat: cria lancamentos

--- front-end/app-gestao/src/app/app.routes.ts
+++ front-end/app-gestao/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Login } from './views/pages/auth/login/login';
 import { SignIn } from './views/pages/auth/sign-in/sign-in';
 import { DefaultLayout } from './layout/default-layout/default-layout';
+import { Lancamentos } from './lancamentos/lancamentos';
 
 
 export const routes: Routes = [
     {path:'login', component:Login},
     {path:'signin', component:SignIn},
     {path:'teste', component:DefaultLayout},
+    {path:'lancamentos', component:Lancamentos},
     {path:'', redirectTo:'login', pathMatch:'full'}
 ];
---
+++ front-end/app-gestao/src/app/lancamentos/lancamentos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamentos',
+  imports: [],
+  templateUrl: './lancamentos.html',
+  styleUrl: './lancamentos.scss',
+})
+export class Lancamentos {
+
+}
